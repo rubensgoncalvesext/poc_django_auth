e7d150f84b7f4d077dc505b491d7e6292fb458da
rb <rubens.goncalves.ext@gmail.com>
2024-04-19 08:50:17 -0300

    zerou touch

--- app/product/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField()
-
-    def __str__(self):
-        return self.name
--- app/product/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Product
-from rest_framework import serializers
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'price')
